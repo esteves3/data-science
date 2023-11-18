ea863cc1e6ec40f8984ff3f266fa583d4a744c37
Carlos Esteves <cmdesteves@gmail.com>
2023-11-18 21:58:30 +0000

    wip

--- credit-score.py
+++ credit-score.py
@@ -6,6 +6,11 @@ from numpy import ndarray
 from dslabs_functions import *
 from matplotlib.pyplot import figure, savefig, show
 from pandas import read_csv, DataFrame
+from numpy import log
+from pandas import Series
+from scipy.stats import norm, expon, lognorm
+from matplotlib.axes import Axes
+from dslabs_functions import plot_multiline_chart
 
 
 filename = "datasets/class_credit_score.csv"
@@ -101,7 +106,7 @@ variables_types: dict[str, list] = get_variable_types(data)
 numeric: list[str] = variables_types["numeric"]
 summary5: DataFrame = data.describe(include="all")
 print(summary5[numeric])
-'''
+
 if [] != numeric:
     # GLOBAL BOXPLOT
     data[numeric].boxplot(rot=45)
@@ -114,10 +119,11 @@ if [] != numeric:
     rows, cols = define_grid(len(numeric))
     fig: Figure
     axs: ndarray
+    '''
     fig, axs = subplots(
         rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT), squeeze=False
     )
-   
+
     i, j = 0, 0
     for n in range(len(numeric)):
         axs[i, j].set_title("Boxplot for %s" % numeric[n])
@@ -141,37 +147,10 @@ if [] != numeric:
 
     savefig(f"images/{file_tag}_outliers_standard.png")
     show()
-
-    # HISTOGRAMAS
-    fig, axs = subplots(
-        rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT), squeeze=False
-    )
-    i: int
-    j: int
-    i, j = 0, 0
-    for n in range(len(numeric)):
-        set_chart_labels(
-            axs[i, j],
-            title=f"Histogram for {numeric[n]}",
-            xlabel=numeric[n],
-            ylabel="nr records",
-        )
-        axs[i, j].hist(data[numeric[n]].dropna().values, "auto")
-        i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
-    savefig(f"images/{file_tag}_single_histograms_numeric.png")
-    show()
-else:
-    print("There are no numeric variables.")
 '''
-
-
-if [] != numeric:
-    rows: int
-    cols: int
-    rows, cols = define_grid(len(numeric))
-    fig, axs = subplots(
-        rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT), squeeze=False
-    )
+    # HISTOGRAMAS
+    fig, axs = subplots(rows, cols, figsize=(
+        cols * HEIGHT, rows * HEIGHT), squeeze=False)
     i: int
     j: int
     i, j = 0, 0
@@ -182,9 +161,53 @@ if [] != numeric:
             xlabel=numeric[n],
             ylabel="nr records",
         )
-        axs[i, j].hist(data[numeric[n]].dropna().values, "auto")
+        axs[i, j].hist(data[numeric[n]].dropna().values, 100)
         i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
     savefig(f"images/{file_tag}_single_histograms_numeric.png")
     show()
+
+    # HISTOGRAMAS W/ DISTRIBUTION
+
+    def compute_known_distributions(x_values: list) -> dict:
+        distributions = dict()
+        # Gaussian
+        mean, sigma = norm.fit(x_values)
+        distributions["Normal(%.1f,%.2f)" % (mean, sigma)
+                      ] = norm.pdf(x_values, mean, sigma)
+        # Exponential
+        loc, scale = expon.fit(x_values)
+        distributions["Exp(%.2f)" % (1 / scale)
+                      ] = expon.pdf(x_values, loc, scale)
+        # LogNorm
+        sigma, loc, scale = lognorm.fit(x_values)
+        distributions["LogNor(%.1f,%.2f)" % (log(scale), sigma)] = lognorm.pdf(
+            x_values, sigma, loc, scale
+        )
+        return distributions
+
+    def histogram_with_distributions(ax: Axes, series: Series, var: str):
+        values: list = series.sort_values().to_list()
+        ax.hist(values, 20, density=True)
+        distributions: dict = compute_known_distributions(values)
+        plot_multiline_chart(
+            values,
+            distributions,
+            ax=ax,
+            title="Best fit for %s" % var,
+            xlabel=var,
+            ylabel="",
+        )
+
+    fig, axs = subplots(
+        rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT), squeeze=False
+    )
+    i, j = 0, 0
+    for n in range(len(numeric)):
+        histogram_with_distributions(
+            axs[i, j], data[numeric[n]].dropna(), numeric[n])
+        i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
+    savefig(f"images/{file_tag}_histogram_numeric_distribution.png")
+    show()
+
 else:
     print("There are no numeric variables.")
