2ddde7fdf1a0dd96e7623cba9a5c50c30f079566
Carlos Esteves <cmdesteves@gmail.com>
2023-11-18 17:59:00 +0000

    Add credit-score

--- credit-score.py
+++ credit-score.py
@@ -1,4 +1,4 @@
-from dslabs_functions import plot_bar_chart
+from dslabs_functions import *
 from matplotlib.pyplot import figure, savefig, show
 from pandas import read_csv, DataFrame
 
@@ -10,10 +10,10 @@ data: DataFrame = read_csv(filename, na_values="", index_col="ID")
 print(data.shape)
 
 
-# SECOND
+# No Records vs No Variables
 
 
-figure(figsize=(4, 2))
+figure(figsize=(4, 4))
 values: dict[str, int] = {
     "nr records": data.shape[0], "nr variables": data.shape[1]}
 
@@ -21,4 +21,40 @@ plot_bar_chart(
     list(values.keys()), list(values.values()), title="Nr of records vs nr variables"
 )
 savefig(f"images/{file_tag}_records_variables.png")
+show()
+
+
+# MISSING VALUES
+mv: dict[str, int] = {}
+for var in data.columns:
+    nr: int = data[var].isna().sum()
+    if nr > 0:
+        mv[var] = nr
+
+figure(figsize=(7, 7))
+plot_bar_chart(
+    list(mv.keys()),
+    list(mv.values()),
+    title="Nr of missing values per variable",
+    xlabel="variables",
+    ylabel="nr missing values",
+)
+savefig(f"images/{file_tag}_mv.png")
+show()
+
+
+# Variables Type
+
+
+variable_types: dict[str, list] = get_variable_types(data)
+print(variable_types)
+counts: dict[str, int] = {}
+for tp in variable_types.keys():
+    counts[tp] = len(variable_types[tp])
+
+figure(figsize=(4, 4))
+plot_bar_chart(
+    list(counts.keys()), list(counts.values()), title="Nr of variables per type"
+)
+savefig(f"images/{file_tag}_variable_types.png")
 show()
